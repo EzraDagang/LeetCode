7f7b9fa981fa18e81c1e540dd7f3d144cf171ce4
EzraDagang <ezradagang@gmail.com>
2026-08-27 13:17:59 +0800

    Understood and self implemented LongestSubstringWithoutRepeatingCharacters_3

--- src/LongestSubstringWithoutRepeatingCharacters_3.java
+++ src/LongestSubstringWithoutRepeatingCharacters_3.java
@@ -1,19 +1,45 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class LongestSubstringWithoutRepeatingCharacters_3 {
     public static int lengthOfLongestSubstring(String s) {
+        if(s == null || s.isEmpty()) return 0;
 
-        if(s == null||s.isEmpty()) return 0;
-
+        int longestSubstring = 0;
         int left = 0;
-        int longestLengthSubstring = 0;
+        Set<Character> window = new HashSet<>();
 
-        for (int right = 0; right <= s.length() - 1; right++){
-            longestLengthSubstring = Math.max(longestLengthSubstring, right - left + 1);
+        for(int right = 0; right <= s.length() - 1; right++){
+               while(window.contains(s.charAt(right))){
+                   window.remove(s.charAt(left));
+                   left++;
+               }
 
-            while( right + 1 <= s.length() - 1 && s.substring(left, right + 1).indexOf(s.charAt(right + 1)) > -1){
-                left++;
-            }
+               window.add(s.charAt(right));
+
+               longestSubstring = Math.max(longestSubstring, right - left + 1);
         }
 
-        return longestLengthSubstring;
+        return longestSubstring;
     }
 }
+
+//======================
+// FIRST OWN IMPLEMENTATION
+//======================
+
+//
+//        if (s == null || s.isEmpty()) return 0;
+//
+//        int left = 0;
+//        int longestLengthSubstring = 0;
+//
+//        for (int right = 0; right <= s.length() - 1; right++) {
+//            longestLengthSubstring = Math.max(longestLengthSubstring, right - left + 1);
+//
+//            while (right + 1 <= s.length() - 1 && s.substring(left, right + 1).indexOf(s.charAt(right + 1)) > -1) {
+//                left++;
+//            }
+//        }
+//
+//        return longestLengthSubstring;
